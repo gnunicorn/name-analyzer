29787292bd3ea377bbe2a13caad9d39ebf0f5686
Benjamin Kampmann <ben.kampmann@googlemail.com>
2009-08-25 18:47:48 +0200

    make it directly executable, smaller changes on names, sloww it so that is looks like it is actually doing something

--- check.py
+++ check.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import urllib2, re
+import urllib2
+import re
+import time
 
 # all the 10 most popular names of the last 5 decades. take from:
 #   http://www.gfds.de/vornamen/beliebteste-vornamen/
@@ -181,14 +183,29 @@ def small_analyzis(names_by_letter, popular_by_letter, local_count, popular_coun
     return local_relatives, popular_relatives
 
 
-def test():
+def full_check(name, sleep_time):
+    print "receiving list of names"
+    time.sleep(sleep_time)
+
     names = get_list_of_names()
+
+    print "reading popular names from the last 5 decades"
+    time.sleep(sleep_time)
+
     popular_names = most_popular_10_names_of_the_last_5_decades.split()
+
+    print "direct analysis"
+    time.sleep(sleep_time * 4)
+
     intersected_count_wo = analyse(names, popular_names)
-    names.add("Benjamin")
+
+    print 
     print "*" * 20
-    print "Now once again, adding ben...."
+    print "Analyzing again, this time including '%s'...." % name
     print "*" * 20
+    time.sleep(sleep_time * 6)
+
+    names.add(name)
     intersected_count_with = analyse(names, popular_names)
     pop_names = len(popular_names)
     percent_per_name = 100. / pop_names
@@ -196,17 +213,38 @@ def test():
     without_percent = intersected_count_wo * percent_per_name
     with_percent = intersected_count_with * percent_per_name
     increase = (100 / without_percent * with_percent) - 100
+
+    print
+    print "calculating end results"
+    time.sleep(sleep_time * 4)
     print
     print
     print "Result - total Popularity"
-    print "w/o Benjamin - with Benjamin"
+    print "w/o %s - with %s" % (name, name)
     print "%s%% - %s%%" % (without_percent, with_percent)
     print "-----------------------------"
+    time.sleep(sleep_time * 2)
     print "%s%% =~ %s%% of your current popularity" % ((with_percent - without_percent), increase)
     print
-    print "With 'Benjamin' you are %s%% more popular than you are now." %  increase
-    print "You should hire him, he is a good investment!"
-
+    time.sleep(sleep_time)
+    print "With '%s' you are %s%% more popular than you are now." % (name, increase)
+    print
+    print "Advice:"
+    time.sleep(sleep_time * 4)
+    if increase == 0:
+        print "\tDoesn't actually matter..."
+    if increase < 0.5:
+        print "\tDo nothing. It is not worth the trouble."
+    elif increase <= 1:
+        print "\tAsk him to do an internship."
+    elif increase <= 2:
+        print "\tSounds about alright. Make some tests of his skills."
+    elif increase <= 3:
+        print "\tUh.. that is good one, you should consider hireing him."
+    elif increase <= 4:
+        print "\tYou should hire him, he is a good investment!"
+    else:
+        print "\tDUDE... WHAT ARE YOU WAITING FOR?"
 
 def graph_urls(local_relatives, popular_relatives):
     url = "http://chart.apis.google.com/chart?chs=900x200&chbh=a&chds=0,25&chco=4d89f9,C6D9fd&chd=t:%s|%s&cht=bvg&chl=a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z" % (','.join([str(x[0]) for x in popular_relatives]), ",".join([str(x[0]) for x in local_relatives]))
@@ -219,7 +257,7 @@ def graph_urls(local_relatives, popular_relatives):
     popular_relatives.reverse()
 
     url = "http://chart.apis.google.com/chart?chs=900x200&chbh=a&chds=0,25&chco=4d89f9,C6D9fd&chd=t:%s|%s&cht=bvg" % (','.join([str(x[0]) for x in popular_relatives]), ",".join([str(x[0]) for x in local_relatives]))
-    print  "want to see the ...", url
+    print  "want to see the shares in comparison:", url
 
 
 def analyse(names, popular_names):
@@ -257,5 +295,13 @@ def analyse(names, popular_names):
 
     return intersected_count
 
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 2:
+        print "Please provide a name as the first and only parameter"
+        sys.exit(1)
 
+    name = sys.argv[1]
+    print "Checking", name
+    full_check(name, 0.5)
 
